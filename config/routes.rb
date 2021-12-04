f022c965bfa6d31bc54afffbf19de3b5dac49e2c
akash.vishwakarma <akash.vishwakarma>
2021-12-04 10:20:02 +0530

    mvp product base

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root "pages#home"
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+end
